13fc151b5c2657f6331876f688ab655230876e9a
joont <pk920207@seeon.kr>
2018-03-17 15:42:39 +0900

    initial commit

---
+++ src/main/java/beadss/Main.java
@@ -0,0 +1,8 @@
+package beadss;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/main/java/mgsssss/Main.java
@@ -0,0 +1,10 @@
+package mgsssss;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
