686772737256f0fee1e20aa8c60559403628ad70
jymoon <juneyoun.moon@navercorp.com>
2018-03-24 03:25:17 +0900

    스텝 1 개선 중

--- src/main/java/beadss/Main.java
+++ src/main/java/beadss/Main.java
@@ -2,7 +2,9 @@ package beadss;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Vector;
@@ -22,16 +24,21 @@ public class Main {
         
         //int amount = scan.nextInt();
         int amount = 14000;
+        Stream<Stream<Integer>> expectedLottoList =
+        		Stream.of(
+	        		Stream.of(new Random().nextInt(44) + 1)
+	        		.distinct()
+					.limit(6)
+					.peek(System.out::println)
+				)
+				.limit(amount/price);
         
-        Vector<List<Integer>> expectedLottoList = new Vector<List<Integer>>(amount/price); 
-        
-        Random random = new Random();
+        /*
         
         for(int i = 0; i < amount/price; i++) {
         	expectedLottoList.add(Stream.generate(() -> random.nextInt(44) + 1).distinct().limit(6).collect(Collectors.toList()));
         }
-        
-        expectedLottoList.stream().forEach(System.out::println);
+        */
         
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         //scan.nextLine();
@@ -44,36 +51,32 @@ public class Main {
         Arrays.asList(correctLottoString.split(","))
         .subList(0, 6)
         .stream()
-        .forEach((numberStr)->{correctLotto.add(Integer.parseInt(numberStr.trim()));});
+        .forEach(numberStr -> {correctLotto.add(Integer.parseInt(numberStr.trim()));});
         
-        int threeCount = 0;
-        int fourCount = 0;
-        int fiveCount = 0;
-        int sixCount = 0;
-        
-        
-        for(List<Integer> lotto : expectedLottoList) {        	
-        	long matchCount = lotto.stream().filter((expectedNumber) -> correctLotto.stream().anyMatch((number) -> number == expectedNumber)).count();
-        	
-        	if(matchCount == 3) {
-        		threeCount++;
-        	} else if(matchCount == 4) {
-        		fourCount++;
-        	} else if(matchCount == 5) {
-        		fiveCount++;
-        	} else if(matchCount == 6) {
-        		sixCount++;
-        	}
-        }
+        Map<Long, List<Long>> result = expectedLottoList
+        .map(
+    		(lotto) -> 
+    			lotto.filter(
+					expectedNumber -> correctLotto.stream().anyMatch((number) -> number == expectedNumber)
+				)
+			.count()
+		)
+        .collect(Collectors.groupingBy((count) -> count));
         
         System.out.println("당첨 통계");
         System.out.println("---------");
-        System.out.println(String.format("3개 일치 (5000원)- %d개", threeCount));
-        System.out.println(String.format("4개 일치 (50000원)- %d개", fourCount));
-        System.out.println(String.format("5개 일치 (150000원)- %d개", fiveCount));
-        System.out.println(String.format("6개 일치 (2000000000원)- %d개", sixCount));
-        System.out.println(String.format("총 수익률은 %d%%입니다.", (threeCount*5000+fourCount*50000+fiveCount*150000+sixCount*2000000000)/amount*100));
         
+        
+        System.out.println(String.format("3개 일치 (5000원)- %d개", getSize(result, 3)));
+        System.out.println(String.format("4개 일치 (50000원)- %d개", getSize(result, 4)));
+        System.out.println(String.format("5개 일치 (150000원)- %d개", getSize(result, 5)));
+        System.out.println(String.format("6개 일치 (2000000000원)- %d개", getSize(result, 6)));
+        System.out.println(String.format("총 수익률은 %d%%입니다.", (getSize(result, 3)*5000+getSize(result, 4)*50000+getSize(result, 5)*150000+getSize(result, 6)*2000000000)/amount*100));
+        
+	}
+	
+	public static <T extends Number, K extends Number> int getSize(Map<T, List<T>> map, K key) {
+		return map.containsKey(key)?map.get(key).size():0;
 	}
 
 }
