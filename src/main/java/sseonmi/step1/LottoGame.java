4177fcfc84cd1b7293b8825ecf5d78d5ea8b6d27
seonmi.shin <seonmi.shin@okiconcession.com>
2018-04-06 17:57:10 +0900

    intellij 설정

--- src/main/java/sseonmi/step1/LottoGame.java
+++ src/main/java/sseonmi/step1/LottoGame.java
@@ -1,6 +1,10 @@
 package sseonmi.step1;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class LottoGame {
     private static final int PRICE = 1000;
