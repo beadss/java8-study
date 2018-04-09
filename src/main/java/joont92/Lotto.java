0feeb90622144a4ecc0c3dfdd0aef44811e81885
ParkJunYoung <pk920207@seeon.kr>
2018-04-09 15:24:42 +0900

    스트림으로 변경

--- src/main/java/joont92/Lotto.java
+++ src/main/java/joont92/Lotto.java
@@ -3,37 +3,33 @@ package joont92;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Lotto {
     final static int NUMBER_COUNT = 6;
     final static int MIN_NUM = 1;
     final static int MAX_NUM = 45;
 
-    private List<Integer> numbers = new ArrayList<Integer>();
+    private List<Integer> numbers;
     private Rank rank = null;
 
     public Lotto() {
-        while(numbers.size() < NUMBER_COUNT){
-            Integer num = (int)(Math.random() * MAX_NUM) + MIN_NUM;
-
-            if(!numbers.contains(num)){
-                numbers.add(num);
-            }
-        }
-
-        Collections.sort(numbers);
+        numbers = Stream.generate(Math::random)
+                .map(i -> (int)(i*MAX_NUM) + MIN_NUM)
+                .distinct()
+                .limit(NUMBER_COUNT)
+                .sorted()
+                .collect(toList());
     }
 
     public void setRank(List<Integer> winningNumber){
-        int hit = 0;
-        for (Integer number : numbers) {
-            for (Integer i : winningNumber) {
-                if(number == i){
-                    hit++;
-                    break;
-                }
-            }
-        }
+        int hit = (int)winningNumber.stream()
+                .filter(numbers::contains)
+                .count();
 
         rank = Rank.getRankFromCount(hit);
     }
@@ -44,33 +40,5 @@ public class Lotto {
 
     public void print(){
         System.out.println(numbers.toString());
-    }
-
-    public static enum Rank{
-        First(6, 2000000000), Second(5, 1500000), Third(4, 50000), Fourth(3, 5000);
-
-        private int hit;
-        private long prizeMoney;
-
-        Rank(int count, long prizeMoney){
-            this.hit = count;
-            this.prizeMoney = prizeMoney;
-        }
-
-        public int getCount() {
-            return hit;
-        }
-        public long getPrizeMoney() {
-            return prizeMoney;
-        }
-
-        public static Rank getRankFromCount(int count){
-            for (Rank rank : Rank.values()) {
-                if(rank.getCount() == count){
-                    return rank;
-                }
-            }
-            return null;
-        }
     }
 }
--- src/main/java/joont92/LottoMachine.java
+++ src/main/java/joont92/LottoMachine.java
@@ -1,6 +1,8 @@
 package joont92;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottoMachine {
     private static final Scanner scanner = new Scanner(System.in);
@@ -17,17 +19,15 @@ public class LottoMachine {
         } else if(money > LIMIT_PRICE){
             System.out.printf("최대 구입금액 : %d\n", LIMIT_PRICE);
         } else{
-            lottoList = new ArrayList<Lotto>();
-
             int count = money / LOTTO_PRICE;
             System.out.printf("%d개를 구매했습니다\n", count);
 
-            for (int i = 0; i < count; i++) {
-                Lotto lotto = new Lotto();
+            lottoList = Stream.generate(Lotto::new)
+                    .limit(count)
+                    .collect(Collectors.toList());
 
-                lottoList.add(lotto);
-                lotto.print();
-            }
+            lottoList.stream()
+                    .forEach(l -> l.print());
         }
 
         return lottoList;
@@ -36,7 +36,17 @@ public class LottoMachine {
     public void setWinningNumber(){
         System.out.println("당첨 번호를 입력해 주세요.");
 
-        winningNumber = new ArrayList<Integer>();
+        String input = scanner.nextLine();
+        // 예외처리 해야함
+        winningNumber = Arrays.stream(input.split(","))
+                .distinct()
+                .limit(6)
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        /*
+        winningNumber = new ArrayList<>();
         do{
             try{
                 Integer num = scanner.nextInt();
@@ -55,43 +65,31 @@ public class LottoMachine {
                 System.out.println("숫자만 입력 가능");
             }
         } while(winningNumber.size() < 6);
+        */
     }
 
     public void draw(List<Lotto> lottoList){
         if (winningNumber == null) {
             System.out.println("아직 추첨 전 입니다.");
         } else{
-            for (Lotto lotto : lottoList) {
-                lotto.setRank(winningNumber);
-            }
+            lottoList.stream()
+                    .forEach(l -> l.setRank(winningNumber));
         }
 
-        Map<String, List<Lotto>> group = new HashMap<String, List<Lotto>>();
-
-        for (Lotto lotto : lottoList) {
-            if (lotto.getRank() != null) {
-                String key = lotto.getRank().name();
-                if(!group.containsKey(key)){
-                    group.put(key, new ArrayList<Lotto>());
-                }
-
-                group.get(key).add(lotto);
-            }
-        }
+        Map<Rank, List<Lotto>> group = lottoList.stream()
+                .filter(l -> l.getRank() != null)
+                .collect(Collectors.groupingBy(Lotto::getRank));
 
         System.out.println("당첨 통계");
         System.out.println("--------------------");
-        long sum = 0;
-        for (Lotto.Rank rank : Lotto.Rank.values()) {
-            if(group.containsKey(rank.name())){
-                List<Lotto> list = group.get(rank.name());
-                System.out.printf("%d개 일치 (%d원) - %d개\n", rank.getCount(), rank.getPrizeMoney(), list.size());
 
-                sum += rank.getPrizeMoney() * list.size();
-            } else{
-                System.out.printf("%d개 일치 (%d원) - 0개\n", rank.getCount(), rank.getPrizeMoney());
-            }
-        }
+        Arrays.stream(Rank.values())
+            .forEach(r -> System.out.printf("%d개 일치 (%d원) - %d개\n", r.getCount(), r.getPrizeMoney(), group.containsKey(r) ? group.get(r).size() : 0));
+
+        long sum = Arrays.stream(Rank.values())
+                .filter(group::containsKey)
+                .mapToLong(r -> r.getPrizeMoney() * group.get(r).size())
+                .sum();
 
         System.out.printf("총 수익률은 %d%%입니다\n", (sum * 100) / (lottoList.size() * LOTTO_PRICE));
     }
