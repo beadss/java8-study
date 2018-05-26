3effedfe3545f7d32320f50eab9bda332dd3178c
jymoon <beadss@nate..com>
2018-05-26 14:06:51 +0900

    step3 구현 완료

--- src/main/java/beadss/LottoGame.java
+++ src/main/java/beadss/LottoGame.java
@@ -1,19 +1,38 @@
 package beadss;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoGame {
-	private static int price = 1000;
+	public static final int PRICE = 1000;
 
 	private List<Lotto> expectedLottoList;
+	private int buyCount;
 
 	public LottoGame(int amount) {
-		this.expectedLottoList = buyLotto(amount);
+		this(amount, new ArrayList<>());
+	}
+
+	public LottoGame(int amount, List<Lotto> manualLottoList) {
+		this.buyCount = amount / PRICE;
+
+		if(manualLottoList.size() > buyCount) {
+			throw new UnsupportedOperationException("돈 부족해요~");
+		}
+
+		final int autoLottoCount = buyCount - manualLottoList.size();
+
+		System.out.println(String.format("수동으로 %d장, 자동으로 %d장을 구매했습니다.", manualLottoList.size(), autoLottoCount));
+
+		this.expectedLottoList = Stream.concat(manualLottoList.stream(), buyLotto(autoLottoCount))
+				.peek(System.out::println)
+				.collect(Collectors.toList());
 	}
 
 	public LottoResult processResult(Lotto correctLotto, int bonusNumber) {
+
 		return expectedLottoList
 				.stream()
 				.map(expected -> correctLotto.match(expected, bonusNumber))
@@ -58,10 +77,8 @@ public class LottoGame {
 		}
 	}
 
-	private List<Lotto> buyLotto(int amount) {
+	private Stream<Lotto> buyLotto(int count) {
 		return Stream.generate(Lotto::generateRandomLotto)
-						.limit(amount/price)
-						.peek(System.out::println)
-						.collect(Collectors.toList());
+						.limit(count);
 	}
 }
--- src/main/java/beadss/Main.java
+++ src/main/java/beadss/Main.java
@@ -1,6 +1,8 @@
 package beadss;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -11,9 +13,22 @@ public class Main {
 
         int amount = scan.nextInt();
 
-		LottoGame game = new LottoGame(amount);
+		System.out.println("수동으로 구매할 로또 수를 입력해주세요.");
+
+		int manualCount = scan.nextInt();
+
+		System.out.println("수동으로 구매할 로또 번호를 입력해주세요.");
+
+		List<Lotto> manualLottoList = new ArrayList<>();
 
 		scan.nextLine();
+
+		for(int i = 0; i < manualCount; i++) {
+			manualLottoList.add(Lotto.parse(scan.nextLine()));
+		}
+
+		LottoGame game = new LottoGame(amount, manualLottoList);
+
         System.out.println("지난 주 당첨 번호를 입력해주세요.");
 
         Lotto correctLotto = Lotto.parse(scan.nextLine());
